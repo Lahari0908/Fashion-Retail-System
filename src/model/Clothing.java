6ee87ef26a41b8f2fa93e2e684b3d1608eb068e3
Lahari0908 <lneravet@gitam.in>
2025-11-17 19:29:02 +1030

    Create Clothing.java

---
+++ src/model/Clothing.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Clothing extends Product {
+    private String size;
+    private String color;
+
+    public Clothing(String productId, String name, double price, String size, String color) {
+        super(productId, name, price);
+        this.size = size;
+        this.color = color;
+    }
+}
