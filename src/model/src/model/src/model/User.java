20ebd49c2e886978cfe31850acddee60b552652a
Lahari0908 <lneravet@gitam.in>
2025-11-17 19:23:52 +1030

    add User.java

---
+++ src/model/src/model/src/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+public abstract class User {
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+}
