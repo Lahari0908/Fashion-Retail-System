cc0dc11b682a3abd9520088a0b88379706c496dc
Lahari0908 <lneravet@gitam.in>
2025-11-19 17:44:02 +1030

    PaymentService.java

--- src/model/PaymentService.java
+++ src/model/PaymentService.java
@@ -2,19 +2,26 @@ package service;
 
 import model.Order;
 
-public class Payment{
-    public Payment authorizePayment(String ORDERNO) {
+public class Payment
+{
+    public Payment authorizePayment(String ORDERNO) 
+    {
         System.out.println("Payment authorisation for " + orderId);
         return new Payment("AUTHORISED");
     }
 
-    public PaymentResult getPayment(Order order) {
+    public PaymentResult getPayment(Order order) 
+    {
         System.out.println("Payment Successful for: $" + order.Total());
         return new PaymentResult("SUCCESSFUL");
     }
 }
 
-class PaymentResult {
+class PaymentResult
+    {
     private String status;
-    public PaymentResult(String status) { this.status = status; }
+    public PaymentResult(String status)
+    { 
+        this.status = status;
+    }
 }
