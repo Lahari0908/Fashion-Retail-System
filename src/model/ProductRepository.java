8bffe95eee2e889b2b39e2a347d2d1755b355e69
Lahari0908 <lneravet@gitam.in>
2025-11-19 17:45:11 +1030

    ProductRepository.java

--- src/model/ProductRepository.java
+++ src/model/ProductRepository.java
@@ -3,14 +3,18 @@ package repository;
 import model.Product;
 
 
-public class ProductRepository {
+public class ProductRepository
+    {
     private List products = new prodducts;
 
-    public List findByCategory(String category) {
+    public List findByCategory(String category)
+        
+    {
         return products;
     }
 
-    public void save(Product product) {
+    public void save(Product product) 
+        {
         products.add(product);
     }
 }
