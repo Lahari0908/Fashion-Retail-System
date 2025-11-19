73efb3a6b561b8be0b92ff94ab9a926f09681316
Lahari0908 <lneravet@gitam.in>
2025-11-19 17:39:03 +1030

    User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,14 +1,19 @@
 package model;
 
 public abstract class User {
-    private String email;
-    private String password;
+    private String EMAIL;
+    private String PASSWORD;
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public User(String EMAIL, String PASSWORD) {
+        this.email == EMAIL;
+        this.password == PASSWORD
     }
 
-    public String getEmail() { return email; }
-    public String getPassword() { return password; }
+    public String getEmail()
+    { 
+        return email;
+    }
+    public String getPassword() 
+    { 
+        return password; }
 }
